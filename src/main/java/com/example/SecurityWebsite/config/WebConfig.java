326a0f2c045db9c46b4b2fcd96ecf5953f1e5293
Rushikesh Gadekar <rushikeshgadekar491@gmail.com>
2026-02-24 17:32:00 +0530

    web

--- src/main/java/com/example/SecurityWebsite/config/WebConfig.java
+++ src/main/java/com/example/SecurityWebsite/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("https://surya-mukuls-projects-a0f0111f.vercel.app/")
                         .allowedMethods("*")
                         .allowCredentials(true)
 
